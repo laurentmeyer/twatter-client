4ccd4ff11a3bb18215cde4f192408d10add47755
lmeyer <laurent.meyer@frontapp.com>
2022-12-05 23:26:46 +0100

    article looks better

--- src/components/article.tsx
+++ src/components/article.tsx
@@ -3,6 +3,7 @@ import ReactMarkdown from 'react-markdown'
 import { ArticleResource } from '../resources/article'
 import styles from '../../styles/markdown-styles.module.css'
 import styled from 'styled-components'
+import Image from 'next/image'
 
 /*
  * Styles.
@@ -10,12 +11,20 @@ import styled from 'styled-components'
 
 const StyledWrapper = styled.div`
   display: grid;
+  padding: 0 20px;
 `
 
 const StyledHeader = styled.h1`
   justify-self: center;
   text-align: center;
-  font-size: 2.4rem;
+  font-size: 2rem;
+`
+
+const StyledSourceLogoWrapper = styled.div`
+  width: 200px;
+  height: 150px;
+  position: relative;
+  justify-self: center;
 `
 
 /*
@@ -30,11 +39,25 @@ interface ArticleProps {
  * Component.
  */
 
-export const Article = ({ article }: ArticleProps) => (
-  <StyledWrapper>
-    <StyledHeader>{article.title}</StyledHeader>
-    <ReactMarkdown className={styles.reactMarkDown}>
-      {article.content}
-    </ReactMarkdown>
-  </StyledWrapper>
-)
+export const Article = ({ article }: ArticleProps) => {
+  const { source } = article
+
+  return (
+    <StyledWrapper>
+      {source && source.logo && (
+        <StyledSourceLogoWrapper>
+          <Image
+            src={source.logo.url}
+            alt={source.logo.alternativeText}
+            fill
+            objectFit="contain"
+          />
+        </StyledSourceLogoWrapper>
+      )}
+      <StyledHeader>{article.title}</StyledHeader>
+      <ReactMarkdown className={styles.reactMarkDown}>
+        {article.content}
+      </ReactMarkdown>
+    </StyledWrapper>
+  )
+}
